16893d0e8c623fa1ca4202537d67e7979779ceae
Nemanja Danev <nemanja.danev.93@gmail.com>
2020-10-13 22:50:04 +0200

    added users model and api for login and registration

--- server/index.js
+++ server/index.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const passport = require('passport');
 const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/jobsdb', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(response => { console.log('MongoDB connected...') })
-    .catch(error => { console.log('MongoDB error...') })
-
 app.use(bodyParser.json());
 app.use(cors());
 
-const jobs = require('./routes/api/jobs');
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
+const users = require('./routes/api/users');
+const jobs = require('./routes/api/jobs');
+app.use('/api/users', users);
 app.use('/api/jobs', jobs);
+
+mongoose.connect('mongodb://localhost:27017/jobsdb', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(response => { console.log('MongoDB connected...') })
+    .catch(error => { console.log('MongoDB error...') })
 
 const port = process.env.PORT || 5000;
 
---
+++ server/routes/api/users.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const key = require('../../config/keys').secret;
+const User = require('../../models/User');
+
+
+/* @route /api/users/register */
+router.post('/register', (req, res) => {
+    let { name, username, email, password, confirm_password } = req.body;
+
+    if (password !== confirm_password) {
+        res.status(400).json({
+            msg: "Password does not matched.",
+
+        });
+    }
+
+    /* Check for username */
+    User.findOne({ username: username }).then(user => {
+        if (user) {
+            res.status(400).json({
+                msg: "User is already taken."
+            });
+        }
+    });
+
+    /* Check for email */
+    User.findOne({ email: email }).then(user => {
+        if (user) {
+            res.status(400).json({
+                msg: "Email is already registerd."
+            });
+        }
+    });
+
+    /* Register a new user */
+    let newUser = new User({
+        name,
+        username,
+        password,
+        email
+    });
+
+    /* Hash the password */
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save().then(user => {
+                return res.status(201).json({
+                    success: true,
+                    msg: "User is now registerd."
+                })
+            });
+        });
+    });
+});
+
+/* @route /api/users/login */
+router.post('/login', (req, res) => {
+    User.findOne({ username: req.body.username }).then(user => {
+        if (!user) {
+            res.status(404).json({
+                msg: "User is not found.",
+                success: false
+            });
+        }
+
+        /* If there is a user go to compare a passowrds */
+        bcrypt.compare(req.body.password, user.password).then(isMatch => {
+            if (isMatch) {
+                const payload = {
+                    _id: user._id,
+                    username: user.username,
+                    name: user.name,
+                    email: user.email
+                }
+
+                /* Add a token to the user using jwt */
+                jwt.sign(payload, key, { expiresIn: "1h" }, (err, token) => {
+                    res.status(200).json({
+                        success: true,
+                        token: `Bearer ${token}`,
+                        msg: "You are logged in."
+                    })
+                });
+
+            } else {
+                res.status(404).json({
+                    msg: "Incorrect password.",
+                    success: false
+                });
+            }
+        });
+    });
+});
+
+/* @route /api/users/profile */
+router.get('/profile', passport.authenticate('jwt', { session: false }), (req, res) => {
+    return res.json({
+        user: req.user
+    });
+});
+
+
+module.exports = router;
